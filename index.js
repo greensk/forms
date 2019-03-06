c520b0b99ee77765624a863eaf571f4cf60264c0
Grigory Kosheev <grigory@kosheev.com>
2019-03-06 15:40:07 +0700

    Добавлен идикатор сохранения

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-document.getElementById('question-save').addEventListener('click', function () {
-	var title = document.getElementById('question-title').value;
-	var question = {'title': title};
-	firebase.database().ref('users/test/form/1').set(question);
-});
+document.getElementById('question-save').addEventListener('click', onSave);
+
+function onSave() {
+	var title = document.getElementById(
+		'question-title'
+	).value
+	var question = {'title': title}
+	firebase.database().ref(
+		'users/test/form/1'
+	).set(
+		question,
+		onSaveComplete
+	)
+	document.getElementById('loader').setAttribute(
+		'style',
+		'display: block'
+	)
+}
+
+function onSaveComplete(err) {
+	if (err) {
+		document.getElementById('message').innerText = 'Ошибка при сохранении'
+	} else {
+		document.getElementById('message').innerText = 'Вопрос сохранен!'
+	}
+	document.getElementById('loader').setAttribute(
+		'style',
+		''
+	)
+}
