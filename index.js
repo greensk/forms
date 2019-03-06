3535ddb76d1678e0b37c324cd7f0b235f3cdf33d
Grigory Kosheev <grigory@kosheev.com>
2019-03-06 17:26:07 +0700

    Информация о себе

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-document.getElementById('question-save').addEventListener('click', onSave);
+document.getElementById('question-save').addEventListener('click', onSave)
+document.getElementById('profile-save').addEventListener('click', onProfileSave)
 
 firebase.database().ref(
 	'users/test/form/1'
 ).on('value', onLoad)
+firebase.database().ref(
+	'users/test/profile'
+).on('value', onProfileLoad)
+
 
 function onSave() {
 	var title = document.getElementById(
@@ -21,16 +26,40 @@ function onSave() {
 	)
 }
 
+function onProfileSave() {
+	var about = document.getElementById(
+		'profile-about'
+	).value
+	var profile = {'about': about}
+	firebase.database().ref(
+		'users/test/profile'
+	).set(
+		profile,
+		onSaveComplete
+	)
+	document.getElementById('loader').setAttribute(
+		'style',
+		'display: block'
+	)
+}
+
 function onLoad (snapshot) {
 	var question = snapshot.val()
 	document.getElementById('question-title').value = question.title
+}
+
+function onProfileLoad (snapshot) {
+	var profile = snapshot.val()
+	if (profile) {
+		document.getElementById('profile-about').value = profile.about
+	}
 }
 
 function onSaveComplete(err) {
 	if (err) {
 		document.getElementById('message').innerText = 'Ошибка при сохранении'
 	} else {
-		document.getElementById('message').innerText = 'Вопрос сохранен!'
+		document.getElementById('message').innerText = 'Сохранено!'
 	}
 	document.getElementById('loader').setAttribute(
 		'style',
